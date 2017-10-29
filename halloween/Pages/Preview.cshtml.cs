e349c8f0157b9ce46e124b31a98a9d248b9c9052
Mia <nezhma@yahoo.com>
2017-10-28 21:44:33 -0700

    .

--- halloween/Pages/Preview.cshtml.cs
+++ halloween/Pages/Preview.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using halloween.Model;
 
 namespace halloween.Pages
 {
@@ -13,5 +14,7 @@ namespace halloween.Pages
         {
 
         }
+
+
     }
 }
